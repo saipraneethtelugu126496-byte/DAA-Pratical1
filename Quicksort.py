54f06d7cc9a7bd0154e21628cd4f7b3f723d1973
saipraneethtelugu126496-byte <saipraneeth.telugu126496@marwadiuniversity.ac.in>
2026-08-05 09:44:35 +0530

    Create Quicksort.py

---
+++ Quicksort.py
@@ -0,0 +1,14 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return left + middle + right
+
+arr = [1, 7, 8, 9, 11, 5]
+sorted_arr = quick_sort(arr)
+print("Sorted array:", sorted_arr)
